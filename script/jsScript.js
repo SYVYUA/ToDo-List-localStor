9c54381a86541ee313079e1d88e18cb7b72fdcd0
SYVY <sivokon94@gmail.com>
2019-06-28 15:30:52 +0300

    do it

--- script/jsScript.js
+++ script/jsScript.js
@@ -1,10 +1,28 @@
 let todoTaskArr = {
-    forDate: Data(),
+    forDate: "",
     forIndex: 1,
     forTextTask: "",
     forButRedact: "button"
 };
-let
+
+//for date
+let date = new Date();
+let curr_date = date.getDate();
+let curr_month = date.getMonth()+1;//+1 бо почина з 0 і випада місяць на 1 менше
+let curr_year = date.getFullYear();
+let curr_hour = date.getHours();
+let curr_minutes = date.getMinutes();
+let curr_sec = date.getSeconds();
+let d = (curr_date + "." + curr_month + "." + curr_year+"<br>"+curr_hour+":"+curr_minutes+":"+curr_sec);
+let divTime = document.createElement("div");
+divTime.style.textAlign = "center";;
+divTime.innerHTML = d;
+//***************************/
+
+let inputField = document.getElementById("inputField").nodeValue;
+let divForTextTask = document.createElement("div");
+divForTextTask.className = "divForTextTask";
 function addNewElement(){
-alert("123");
+    todoTaskArr.forDate = d;
+    
 }
