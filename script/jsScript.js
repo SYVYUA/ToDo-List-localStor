13b73d7c4e40c1f5520d78819480b4ce831a4bc9
SYVY <sivokon94@gmail.com>
2019-06-28 16:28:32 +0300

    123

--- script/jsScript.js
+++ script/jsScript.js
@@ -5,7 +5,7 @@ let todoTaskArr = {
     forButRedact: "button"
 };
 
-//for date
+  //for date
 let date = new Date();
 let curr_date = date.getDate();
 let curr_month = date.getMonth()+1;//+1 бо почина з 0 і випада місяць на 1 менше
@@ -15,14 +15,24 @@ let curr_minutes = date.getMinutes();
 let curr_sec = date.getSeconds();
 let d = (curr_date + "." + curr_month + "." + curr_year+"<br>"+curr_hour+":"+curr_minutes+":"+curr_sec);
 let divTime = document.createElement("div");
-divTime.style.textAlign = "center";;
+divTime.style.textAlign = "center";
 divTime.innerHTML = d;
+
 //***************************/
 
-let inputField = document.getElementById("inputField").nodeValue;
-let divForTextTask = document.createElement("div");
-divForTextTask.className = "divForTextTask";
+
+
 function addNewElement(){
-    todoTaskArr.forDate = d;
+    let inputField = document.getElementById("inputField").value;
+    let divForTextTask = document.createElement("li");
+    divForTextTask.className = "todoItem";
+    divForTextTask.innerHTML = inputField;
     
-}
+    if(inputField ==""){
+        alert("You must write something")
+    }else{
+        document.body.appendChild(divForTextTask);
+    }
+    document.getElementById("inputField").value = "";
+
+};
