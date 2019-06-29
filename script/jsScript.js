45805b37634f75ea97d3cf74b8df49c4c8e8a23f
SYVY <sivokon94@gmail.com>
2019-06-30 00:08:56 +0300

    date

--- script/jsScript.js
+++ script/jsScript.js
@@ -5,6 +5,7 @@ function toLocal(){
 	localStorage.setItem("todos",todos);
 }
 
+
 list.addEventListener('click', function (ev) {
     if(ev.target.tagName === "LI") {
        ev.target.classList.toggle('checked');
@@ -17,13 +18,27 @@ list.addEventListener('click', function (ev) {
 }, false);
 
 function newElement() {
-    let li = document.createElement('li');
+  //for date
+  let date = new Date();
+  let curr_date = date.getDate();
+  let curr_month = date.getMonth()+1;//+1 бо почина з 0 і випада місяць на 1 менше
+  let curr_year = date.getFullYear();
+  let curr_hour = date.getHours();
+  let curr_minutes = date.getMinutes();
+  let cur_second = date.getSeconds();
+  var d = (curr_date + "." + curr_month + "." + curr_year+"<br>"+curr_hour+":"+curr_minutes+":"+cur_second+" ");
+  let divTime = document.createElement("div");
+  divTime.style.textAlign = "left";
+  divTime.innerHTML = d;
+//***************************/
+    let li = document.createElement("li");
     let inputValue = document.getElementById('toDoEl').value;
     let t = document.createTextNode(inputValue);
     li.appendChild(t);
     if(inputValue == "") {
-       alert("Введите ваше дело!");
+       alert("Field must not to be empty!");
     } else{
+       document.getElementById('list').appendChild(divTime);
        document.getElementById('list').appendChild(li);
     }
     document.getElementById('toDoEl').value = "";
@@ -32,7 +47,7 @@ function newElement() {
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
-	toLocal();
+	  toLocal();
 }
 
 if(localStorage.getItem("todos")){
